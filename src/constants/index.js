e29b6b6b4bde72a0c48317c833c6613a7a70b6a7
tracy kendrick <tracykendrick0@gmail.com>
2023-10-03 18:26:15 -0400

    Hero and about sections completed

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,15 +46,15 @@ const services = [
     icon: web,
   },
   {
-    title: "React Native Developer",
+    title: "React Developer",
     icon: mobile,
   },
   {
-    title: "Backend Developer",
+    title: "Frontend Developer",
     icon: backend,
   },
   {
-    title: "Content Creator",
+    title: "Game Developer",
     icon: creator,
   },
 ];
