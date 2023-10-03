0be5812818339e3aa4af9b6a603b2e6e3e27b477
tracy kendrick <tracykendrick0@gmail.com>
2023-10-03 14:24:27 -0400

    did the About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a skilled software developer with a strong track record of
+        designing and implementing innovative solutions to complex problems,
+        leveraging my expertise in programming languages, software architecture,
+        and problem-solving to deliver high-quality software.
+      </motion.p>
+    </>
+  );
+};
 
-export default About
+export default About;
