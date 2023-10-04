12b5c2a96e7a38842c3626923ef4221b2a65cec8
tracy kendrick <tracykendrick0@gmail.com>
2023-10-04 11:21:40 -0400

    navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { thislogo, menu, close } from "../assets";
@@ -26,8 +27,11 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-purple : bg-transparent
-    `}
+      className={`${
+        styles.paddingX
+      } w-full flex items-center py-5 fixed top-0 z-20 ${
+        scrolled ? "bg-primary" : "bg-transparent"
+      }`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -38,12 +42,13 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img src={thislogo} alt="logo" className="w-9 h-9 object-contain " />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Tracy Kendrick{" "}
+          <img src={thislogo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+            Tracy Kendrick &nbsp;
             <span className="sm:block hidden"> | Frontend Developer</span>
           </p>
         </Link>
+
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((nav) => (
             <li
